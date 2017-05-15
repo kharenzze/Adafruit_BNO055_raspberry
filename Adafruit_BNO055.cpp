c0e20a21bc39fb492df8f9a7f358c85a02aeead5
Kharenzze <str237@gmail.com>
2017-05-15 23:07:36 +0000

    Init repo

---
+++ Adafruit_BNO055.cpp
@@ -0,0 +1,253 @@
+/***************************************************************************
+  This is a library for the BNO055 orientation sensor
+
+  Designed specifically to work with the Adafruit BNO055 Breakout.
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/products
+
+  These sensors use I2C to communicate, 2 pins are required to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by KTOWN for Adafruit Industries.
+
+  MIT license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+// #if ARDUINO >= 100
+//  #include "Arduino.h"
+// #else
+//  #include "WProgram.h"
+// #endif
+
+#include <math.h>
+#include <limits.h>
+
+#include "Adafruit_BNO055.h"
+
+/***************************************************************************
+ CONSTRUCTOR
+ ***************************************************************************/
+
+/**************************************************************************/
+/*!
+    @brief  Instantiates a new Adafruit_BNO055 class
+*/
+/**************************************************************************/
+Adafruit_BNO055::Adafruit_BNO055(int32_t sensorID, uint8_t address)
+{
+  _sensorID = sensorID;
+  _address = address;
+}
+
+/***************************************************************************
+ PUBLIC FUNCTIONS
+ ***************************************************************************/
+
+/**************************************************************************/
+/*!
+    @brief  Sets up the HW
+*/
+/**************************************************************************/
+bool Adafruit_BNO055::begin(adafruit_bno055_opmode_t mode)
+{
+  return true;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Puts the chip in the specified operating mode
+*/
+/**************************************************************************/
+void Adafruit_BNO055::setMode(adafruit_bno055_opmode_t mode)
+{
+
+}
+
+/**************************************************************************/
+/*!
+    @brief  Use the external 32.768KHz crystal
+*/
+/**************************************************************************/
+void Adafruit_BNO055::setExtCrystalUse(bool usextal)
+{
+
+}
+
+
+/**************************************************************************/
+/*!
+    @brief  Gets the latest system status info
+*/
+/**************************************************************************/
+void Adafruit_BNO055::getSystemStatus(uint8_t *system_status, uint8_t *self_test_result, uint8_t *system_error)
+{
+
+}
+
+/**************************************************************************/
+/*!
+    @brief  Gets the chip revision numbers
+*/
+/**************************************************************************/
+void Adafruit_BNO055::getRevInfo(adafruit_bno055_rev_info_t* info)
+{
+
+}
+
+/**************************************************************************/
+/*!
+    @brief  Gets current calibration state.  Each value should be a uint8_t
+            pointer and it will be set to 0 if not calibrated and 3 if
+            fully calibrated.
+*/
+/**************************************************************************/
+void Adafruit_BNO055::getCalibration(uint8_t* sys, uint8_t* gyro, uint8_t* accel, uint8_t* mag) {
+
+}
+
+/**************************************************************************/
+/*!
+    @brief  Gets the temperature in degrees celsius
+*/
+/**************************************************************************/
+int8_t Adafruit_BNO055::getTemp(void)
+{
+  int8_t temp = (int8_t)(read8(BNO055_TEMP_ADDR));
+  return temp;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Gets a vector reading from the specified source
+*/
+/**************************************************************************/
+imu::Vector<3> Adafruit_BNO055::getVector(adafruit_vector_type_t vector_type)
+{
+  imu::Vector<3> xyz;
+  return xyz;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Gets a quaternion reading from the specified source
+*/
+/**************************************************************************/
+imu::Quaternion Adafruit_BNO055::getQuat(void)
+{
+  int16_t x, y, z, w ;
+  x = y = z = w = 0;
+
+  /* Assign to Quaternion */
+  /* See http://ae-bst.resource.bosch.com/media/products/dokumente/bno055/BST_BNO055_DS000_12~1.pdf
+     3.6.5.5 Orientation (Quaternion)  */
+  const double scale = (1.0 / (1<<14));
+  imu::Quaternion quat(scale * w, scale * x, scale * y, scale * z);
+  return quat;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Provides the sensor_t data for this sensor
+*/
+/**************************************************************************/
+void Adafruit_BNO055::getSensor(sensor_t *sensor)
+{
+
+}
+
+/**************************************************************************/
+/*!
+    @brief  Reads the sensor and returns the data as a sensors_event_t
+*/
+/**************************************************************************/
+bool Adafruit_BNO055::getEvent(sensors_event_t *event)
+{
+
+  return true;
+}
+
+/**************************************************************************/
+/*!
+@brief  Reads the sensor's offset registers into a byte array
+*/
+/**************************************************************************/
+bool Adafruit_BNO055::getSensorOffsets(uint8_t* calibData)
+{
+    return false;
+}
+
+/**************************************************************************/
+/*!
+@brief  Reads the sensor's offset registers into an offset struct
+*/
+/**************************************************************************/
+bool Adafruit_BNO055::getSensorOffsets(adafruit_bno055_offsets_t &offsets_type)
+{
+    return false;
+}
+
+
+/**************************************************************************/
+/*!
+@brief  Writes an array of calibration values to the sensor's offset registers
+*/
+/**************************************************************************/
+void Adafruit_BNO055::setSensorOffsets(const uint8_t* calibData)
+{
+
+}
+
+/**************************************************************************/
+/*!
+@brief  Writes to the sensor's offset registers from an offset struct
+*/
+/**************************************************************************/
+void Adafruit_BNO055::setSensorOffsets(const adafruit_bno055_offsets_t &offsets_type)
+{
+
+}
+
+bool Adafruit_BNO055::isFullyCalibrated(void)
+{
+    return true;
+}
+
+
+/***************************************************************************
+ PRIVATE FUNCTIONS
+ ***************************************************************************/
+
+/**************************************************************************/
+/*!
+    @brief  Writes an 8 bit value over I2C
+*/
+/**************************************************************************/
+bool Adafruit_BNO055::write8(adafruit_bno055_reg_t reg, uint8_t value)
+{
+  return true;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Reads an 8 bit value over I2C
+*/
+/**************************************************************************/
+uint8_t Adafruit_BNO055::read8(adafruit_bno055_reg_t reg )
+{
+  uint8_t value = 0;
+  return value;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Reads the specified number of bytes over I2C
+*/
+/**************************************************************************/
+bool Adafruit_BNO055::readLen(adafruit_bno055_reg_t reg, uint8_t * buffer, uint8_t len)
+{
+  return true;
+}
