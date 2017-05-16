35d81206eda67839ca6cde599ef63a22fe4ea412
Kharenzze <str237@gmail.com>
2017-05-16 23:09:01 +0000

    Defined all methods

--- Adafruit_BNO055.cpp
+++ Adafruit_BNO055.cpp
@@ -107,7 +107,9 @@ bool Adafruit_BNO055::begin(adafruit_bno055_opmode_t mode)
 /**************************************************************************/
 void Adafruit_BNO055::setMode(adafruit_bno055_opmode_t mode)
 {
-
+  _mode = mode;
+  write8(BNO055_OPR_MODE_ADDR, _mode);
+  usleep(30000);
 }
 
 /**************************************************************************/
@@ -117,7 +119,21 @@ void Adafruit_BNO055::setMode(adafruit_bno055_opmode_t mode)
 /**************************************************************************/
 void Adafruit_BNO055::setExtCrystalUse(bool usextal)
 {
+  adafruit_bno055_opmode_t modeback = _mode;
 
+  /* Switch to config mode (just in case since this is the default) */
+  setMode(OPERATION_MODE_CONFIG);
+  usleep(25000);
+  write8(BNO055_PAGE_ID_ADDR, 0);
+  if (usextal) {
+    write8(BNO055_SYS_TRIGGER_ADDR, 0x80);
+  } else {
+    write8(BNO055_SYS_TRIGGER_ADDR, 0x00);
+  }
+  usleep(10000);
+  /* Set the requested operating mode (see section 3.3) */
+  setMode(modeback);
+  usleep(20000);
 }
 
 
@@ -128,7 +144,53 @@ void Adafruit_BNO055::setExtCrystalUse(bool usextal)
 /**************************************************************************/
 void Adafruit_BNO055::getSystemStatus(uint8_t *system_status, uint8_t *self_test_result, uint8_t *system_error)
 {
+  write8(BNO055_PAGE_ID_ADDR, 0);
 
+  /* System Status (see section 4.3.58)
+     ---------------------------------
+     0 = Idle
+     1 = System Error
+     2 = Initializing Peripherals
+     3 = System Iniitalization
+     4 = Executing Self-Test
+     5 = Sensor fusio algorithm running
+     6 = System running without fusion algorithms */
+
+  if (system_status != 0)
+    *system_status    = read8(BNO055_SYS_STAT_ADDR);
+
+  /* Self Test Results (see section )
+     --------------------------------
+     1 = test passed, 0 = test failed
+
+     Bit 0 = Accelerometer self test
+     Bit 1 = Magnetometer self test
+     Bit 2 = Gyroscope self test
+     Bit 3 = MCU self test
+
+     0x0F = all good! */
+
+  if (self_test_result != 0)
+    *self_test_result = read8(BNO055_SELFTEST_RESULT_ADDR);
+
+  /* System Error (see section 4.3.59)
+     ---------------------------------
+     0 = No error
+     1 = Peripheral initialization error
+     2 = System initialization error
+     3 = Self test result failed
+     4 = Register map value out of range
+     5 = Register map address out of range
+     6 = Register map write error
+     7 = BNO low power mode not available for selected operat ion mode
+     8 = Accelerometer power mode not available
+     9 = Fusion algorithm configuration error
+     A = Sensor configuration error */
+
+  if (system_error != 0)
+    *system_error     = read8(BNO055_SYS_ERR_ADDR);
+
+  usleep(200000);
 }
 
 /**************************************************************************/
@@ -138,7 +200,25 @@ void Adafruit_BNO055::getSystemStatus(uint8_t *system_status, uint8_t *self_test
 /**************************************************************************/
 void Adafruit_BNO055::getRevInfo(adafruit_bno055_rev_info_t* info)
 {
+  uint8_t a, b;
 
+  memset(info, 0, sizeof(adafruit_bno055_rev_info_t));
+
+  /* Check the accelerometer revision */
+  info->accel_rev = read8(BNO055_ACCEL_REV_ID_ADDR);
+
+  /* Check the magnetometer revision */
+  info->mag_rev   = read8(BNO055_MAG_REV_ID_ADDR);
+
+  /* Check the gyroscope revision */
+  info->gyro_rev  = read8(BNO055_GYRO_REV_ID_ADDR);
+
+  /* Check the SW revision */
+  info->bl_rev    = read8(BNO055_BL_REV_ID_ADDR);
+
+  a = read8(BNO055_SW_REV_ID_LSB_ADDR);
+  b = read8(BNO055_SW_REV_ID_MSB_ADDR);
+  info->sw_rev = (((uint16_t)b) << 8) | ((uint16_t)a);
 }
 
 /**************************************************************************/
@@ -149,7 +229,19 @@ void Adafruit_BNO055::getRevInfo(adafruit_bno055_rev_info_t* info)
 */
 /**************************************************************************/
 void Adafruit_BNO055::getCalibration(uint8_t* sys, uint8_t* gyro, uint8_t* accel, uint8_t* mag) {
-
+  uint8_t calData = read8(BNO055_CALIB_STAT_ADDR);
+  if (sys != NULL) {
+    *sys = (calData >> 6) & 0x03;
+  }
+  if (gyro != NULL) {
+    *gyro = (calData >> 4) & 0x03;
+  }
+  if (accel != NULL) {
+    *accel = (calData >> 2) & 0x03;
+  }
+  if (mag != NULL) {
+    *mag = calData & 0x03;
+  }
 }
 
 /**************************************************************************/
@@ -171,6 +263,50 @@ int8_t Adafruit_BNO055::getTemp(void)
 imu::Vector<3> Adafruit_BNO055::getVector(adafruit_vector_type_t vector_type)
 {
   imu::Vector<3> xyz;
+  uint8_t buffer[6];
+  memset (buffer, 0, 6);
+
+  int16_t x, y, z;
+  x = y = z = 0;
+
+  /* Read vector data (6 bytes) */
+  readLen((adafruit_bno055_reg_t)vector_type, buffer, 6);
+
+  x = ((int16_t)buffer[0]) | (((int16_t)buffer[1]) << 8);
+  y = ((int16_t)buffer[2]) | (((int16_t)buffer[3]) << 8);
+  z = ((int16_t)buffer[4]) | (((int16_t)buffer[5]) << 8);
+
+  /* Convert the value to an appropriate range (section 3.6.4) */
+  /* and assign the value to the Vector type */
+  switch (vector_type) {
+    case VECTOR_MAGNETOMETER:
+      /* 1uT = 16 LSB */
+      xyz[0] = ((double)x)/16.0;
+      xyz[1] = ((double)y)/16.0;
+      xyz[2] = ((double)z)/16.0;
+      break;
+    case VECTOR_GYROSCOPE:
+      /* 1dps = 16 LSB */
+      xyz[0] = ((double)x)/16.0;
+      xyz[1] = ((double)y)/16.0;
+      xyz[2] = ((double)z)/16.0;
+      break;
+    case VECTOR_EULER:
+      /* 1 degree = 16 LSB */
+      xyz[0] = ((double)x)/16.0;
+      xyz[1] = ((double)y)/16.0;
+      xyz[2] = ((double)z)/16.0;
+      break;
+    case VECTOR_ACCELEROMETER:
+    case VECTOR_LINEARACCEL:
+    case VECTOR_GRAVITY:
+      /* 1m/s^2 = 100 LSB */
+      xyz[0] = ((double)x)/100.0;
+      xyz[1] = ((double)y)/100.0;
+      xyz[2] = ((double)z)/100.0;
+      break;
+  }
+
   return xyz;
 }
 
@@ -181,8 +317,18 @@ imu::Vector<3> Adafruit_BNO055::getVector(adafruit_vector_type_t vector_type)
 /**************************************************************************/
 imu::Quaternion Adafruit_BNO055::getQuat(void)
 {
-  int16_t x, y, z, w ;
+  uint8_t buffer[8];
+  memset (buffer, 0, 8);
+
+  int16_t x, y, z, w;
   x = y = z = w = 0;
+
+  /* Read quat data (8 bytes) */
+  readLen(BNO055_QUATERNION_DATA_W_LSB_ADDR, buffer, 8);
+  w = (((uint16_t)buffer[1]) << 8) | ((uint16_t)buffer[0]);
+  x = (((uint16_t)buffer[3]) << 8) | ((uint16_t)buffer[2]);
+  y = (((uint16_t)buffer[5]) << 8) | ((uint16_t)buffer[4]);
+  z = (((uint16_t)buffer[7]) << 8) | ((uint16_t)buffer[6]);
 
   /* Assign to Quaternion */
   /* See http://ae-bst.resource.bosch.com/media/products/dokumente/bno055/BST_BNO055_DS000_12~1.pdf
@@ -199,7 +345,19 @@ imu::Quaternion Adafruit_BNO055::getQuat(void)
 /**************************************************************************/
 void Adafruit_BNO055::getSensor(sensor_t *sensor)
 {
+  /* Clear the sensor_t object */
+  memset(sensor, 0, sizeof(sensor_t));
 
+  /* Insert the sensor name in the fixed length char array */
+  strncpy (sensor->name, "BNO055", sizeof(sensor->name) - 1);
+  sensor->name[sizeof(sensor->name)- 1] = 0;
+  sensor->version     = 1;
+  sensor->sensor_id   = _sensorID;
+  sensor->type        = SENSOR_TYPE_ORIENTATION;
+  sensor->min_delay   = 0;
+  sensor->max_value   = 0.0F;
+  sensor->min_value   = 0.0F;
+  sensor->resolution  = 0.01F;
 }
 
 /**************************************************************************/
@@ -209,6 +367,19 @@ void Adafruit_BNO055::getSensor(sensor_t *sensor)
 /**************************************************************************/
 bool Adafruit_BNO055::getEvent(sensors_event_t *event)
 {
+  /* Clear the event */
+  memset(event, 0, sizeof(sensors_event_t));
+
+  event->version   = sizeof(sensors_event_t);
+  event->sensor_id = _sensorID;
+  event->type      = SENSOR_TYPE_ORIENTATION;
+  event->timestamp = millis();
+
+  /* Get a Euler angle sample for orientation */
+  imu::Vector<3> euler = getVector(Adafruit_BNO055::VECTOR_EULER);
+  event->orientation.x = euler.x();
+  event->orientation.y = euler.y();
+  event->orientation.z = euler.z();
 
   return true;
 }
@@ -220,7 +391,17 @@ bool Adafruit_BNO055::getEvent(sensors_event_t *event)
 /**************************************************************************/
 bool Adafruit_BNO055::getSensorOffsets(uint8_t* calibData)
 {
-    return false;
+  if (isFullyCalibrated())
+  {
+    adafruit_bno055_opmode_t lastMode = _mode;
+    setMode(OPERATION_MODE_CONFIG);
+
+    readLen(ACCEL_OFFSET_X_LSB_ADDR, calibData, NUM_BNO055_OFFSET_REGISTERS);
+
+    setMode(lastMode);
+    return true;
+  }
+  return false;
 }
 
 /**************************************************************************/
@@ -230,6 +411,30 @@ bool Adafruit_BNO055::getSensorOffsets(uint8_t* calibData)
 /**************************************************************************/
 bool Adafruit_BNO055::getSensorOffsets(adafruit_bno055_offsets_t &offsets_type)
 {
+  if (isFullyCalibrated())
+    {
+      adafruit_bno055_opmode_t lastMode = _mode;
+      setMode(OPERATION_MODE_CONFIG);
+      usleep(2000);
+
+      offsets_type.accel_offset_x = (read8(ACCEL_OFFSET_X_MSB_ADDR) << 8) | (read8(ACCEL_OFFSET_X_LSB_ADDR));
+      offsets_type.accel_offset_y = (read8(ACCEL_OFFSET_Y_MSB_ADDR) << 8) | (read8(ACCEL_OFFSET_Y_LSB_ADDR));
+      offsets_type.accel_offset_z = (read8(ACCEL_OFFSET_Z_MSB_ADDR) << 8) | (read8(ACCEL_OFFSET_Z_LSB_ADDR));
+
+      offsets_type.gyro_offset_x = (read8(GYRO_OFFSET_X_MSB_ADDR) << 8) | (read8(GYRO_OFFSET_X_LSB_ADDR));
+      offsets_type.gyro_offset_y = (read8(GYRO_OFFSET_Y_MSB_ADDR) << 8) | (read8(GYRO_OFFSET_Y_LSB_ADDR));
+      offsets_type.gyro_offset_z = (read8(GYRO_OFFSET_Z_MSB_ADDR) << 8) | (read8(GYRO_OFFSET_Z_LSB_ADDR));
+
+      offsets_type.mag_offset_x = (read8(MAG_OFFSET_X_MSB_ADDR) << 8) | (read8(MAG_OFFSET_X_LSB_ADDR));
+      offsets_type.mag_offset_y = (read8(MAG_OFFSET_Y_MSB_ADDR) << 8) | (read8(MAG_OFFSET_Y_LSB_ADDR));
+      offsets_type.mag_offset_z = (read8(MAG_OFFSET_Z_MSB_ADDR) << 8) | (read8(MAG_OFFSET_Z_LSB_ADDR));
+
+      offsets_type.accel_radius = (read8(ACCEL_RADIUS_MSB_ADDR) << 8) | (read8(ACCEL_RADIUS_LSB_ADDR));
+      offsets_type.mag_radius = (read8(MAG_RADIUS_MSB_ADDR) << 8) | (read8(MAG_RADIUS_LSB_ADDR));
+
+      setMode(lastMode);
+      return true;
+    }
     return false;
 }
 
@@ -241,7 +446,39 @@ bool Adafruit_BNO055::getSensorOffsets(adafruit_bno055_offsets_t &offsets_type)
 /**************************************************************************/
 void Adafruit_BNO055::setSensorOffsets(const uint8_t* calibData)
 {
+  adafruit_bno055_opmode_t lastMode = _mode;
+  setMode(OPERATION_MODE_CONFIG);
+  usleep(25000);
 
+  /* A writeLen() would make this much cleaner */
+  write8(ACCEL_OFFSET_X_LSB_ADDR, calibData[0]);
+  write8(ACCEL_OFFSET_X_MSB_ADDR, calibData[1]);
+  write8(ACCEL_OFFSET_Y_LSB_ADDR, calibData[2]);
+  write8(ACCEL_OFFSET_Y_MSB_ADDR, calibData[3]);
+  write8(ACCEL_OFFSET_Z_LSB_ADDR, calibData[4]);
+  write8(ACCEL_OFFSET_Z_MSB_ADDR, calibData[5]);
+
+  write8(GYRO_OFFSET_X_LSB_ADDR, calibData[6]);
+  write8(GYRO_OFFSET_X_MSB_ADDR, calibData[7]);
+  write8(GYRO_OFFSET_Y_LSB_ADDR, calibData[8]);
+  write8(GYRO_OFFSET_Y_MSB_ADDR, calibData[9]);
+  write8(GYRO_OFFSET_Z_LSB_ADDR, calibData[10]);
+  write8(GYRO_OFFSET_Z_MSB_ADDR, calibData[11]);
+
+  write8(MAG_OFFSET_X_LSB_ADDR, calibData[12]);
+  write8(MAG_OFFSET_X_MSB_ADDR, calibData[13]);
+  write8(MAG_OFFSET_Y_LSB_ADDR, calibData[14]);
+  write8(MAG_OFFSET_Y_MSB_ADDR, calibData[15]);
+  write8(MAG_OFFSET_Z_LSB_ADDR, calibData[16]);
+  write8(MAG_OFFSET_Z_MSB_ADDR, calibData[17]);
+
+  write8(ACCEL_RADIUS_LSB_ADDR, calibData[18]);
+  write8(ACCEL_RADIUS_MSB_ADDR, calibData[19]);
+
+  write8(MAG_RADIUS_LSB_ADDR, calibData[20]);
+  write8(MAG_RADIUS_MSB_ADDR, calibData[21]);
+
+  setMode(lastMode);
 }
 
 /**************************************************************************/
@@ -251,12 +488,47 @@ void Adafruit_BNO055::setSensorOffsets(const uint8_t* calibData)
 /**************************************************************************/
 void Adafruit_BNO055::setSensorOffsets(const adafruit_bno055_offsets_t &offsets_type)
 {
+  adafruit_bno055_opmode_t lastMode = _mode;
+  setMode(OPERATION_MODE_CONFIG);
+  usleep(25000);
 
+  write8(ACCEL_OFFSET_X_LSB_ADDR, (offsets_type.accel_offset_x) & 0x0FF);
+  write8(ACCEL_OFFSET_X_MSB_ADDR, (offsets_type.accel_offset_x >> 8) & 0x0FF);
+  write8(ACCEL_OFFSET_Y_LSB_ADDR, (offsets_type.accel_offset_y) & 0x0FF);
+  write8(ACCEL_OFFSET_Y_MSB_ADDR, (offsets_type.accel_offset_y >> 8) & 0x0FF);
+  write8(ACCEL_OFFSET_Z_LSB_ADDR, (offsets_type.accel_offset_z) & 0x0FF);
+  write8(ACCEL_OFFSET_Z_MSB_ADDR, (offsets_type.accel_offset_z >> 8) & 0x0FF);
+
+  write8(GYRO_OFFSET_X_LSB_ADDR, (offsets_type.gyro_offset_x) & 0x0FF);
+  write8(GYRO_OFFSET_X_MSB_ADDR, (offsets_type.gyro_offset_x >> 8) & 0x0FF);
+  write8(GYRO_OFFSET_Y_LSB_ADDR, (offsets_type.gyro_offset_y) & 0x0FF);
+  write8(GYRO_OFFSET_Y_MSB_ADDR, (offsets_type.gyro_offset_y >> 8) & 0x0FF);
+  write8(GYRO_OFFSET_Z_LSB_ADDR, (offsets_type.gyro_offset_z) & 0x0FF);
+  write8(GYRO_OFFSET_Z_MSB_ADDR, (offsets_type.gyro_offset_z >> 8) & 0x0FF);
+
+  write8(MAG_OFFSET_X_LSB_ADDR, (offsets_type.mag_offset_x) & 0x0FF);
+  write8(MAG_OFFSET_X_MSB_ADDR, (offsets_type.mag_offset_x >> 8) & 0x0FF);
+  write8(MAG_OFFSET_Y_LSB_ADDR, (offsets_type.mag_offset_y) & 0x0FF);
+  write8(MAG_OFFSET_Y_MSB_ADDR, (offsets_type.mag_offset_y >> 8) & 0x0FF);
+  write8(MAG_OFFSET_Z_LSB_ADDR, (offsets_type.mag_offset_z) & 0x0FF);
+  write8(MAG_OFFSET_Z_MSB_ADDR, (offsets_type.mag_offset_z >> 8) & 0x0FF);
+
+  write8(ACCEL_RADIUS_LSB_ADDR, (offsets_type.accel_radius) & 0x0FF);
+  write8(ACCEL_RADIUS_MSB_ADDR, (offsets_type.accel_radius >> 8) & 0x0FF);
+
+  write8(MAG_RADIUS_LSB_ADDR, (offsets_type.mag_radius) & 0x0FF);
+  write8(MAG_RADIUS_MSB_ADDR, (offsets_type.mag_radius >> 8) & 0x0FF);
+
+  setMode(lastMode);
 }
 
 bool Adafruit_BNO055::isFullyCalibrated(void)
 {
-    return true;
+  uint8_t system, gyro, accel, mag;
+  getCalibration(&system, &gyro, &accel, &mag);
+  if (system < 3 || gyro < 3 || accel < 3 || mag < 3)
+      return false;
+  return true;
 }
 
 
